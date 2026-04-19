1d3febbec74b6b92546384b4f9b6bb97c53540d7
thomas-seguin <seguinthomas7@gmail.com>
2026-04-19 09:59:44 -0400

    add support for loading shaders from file

--- JanusEngine/src/Janus/Renderer/Shader.cpp
+++ JanusEngine/src/Janus/Renderer/Shader.cpp
@@ -5,6 +5,18 @@
 #include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Janus {
+	Shader* Shader::Create(const std::string& filepath) {
+		switch (Renderer::GetAPI()) {
+		case RendererAPI::API::None:
+			JN_CORE_ASSERT(false, "RendererAPI::None is current not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return new OpenGLShader(filepath);
+		}
+
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc) {
 		switch (Renderer::GetAPI()) {
 		case RendererAPI::API::None:

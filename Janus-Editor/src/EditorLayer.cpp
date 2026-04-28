7b95b8bc00acb42189fe6e0bad202add1c2f6f4b
thomas-seguin <seguinthomas7@gmail.com>
2026-04-28 11:16:58 -0400

    started work on camera component UI

--- Janus-Editor/src/EditorLayer.cpp
+++ Janus-Editor/src/EditorLayer.cpp
@@ -20,8 +20,11 @@ namespace Janus {
 
 		m_ActiveScene = CreateRef<Scene>();
 
-		auto square = m_ActiveScene->CreateEntity("Square");
+		auto square = m_ActiveScene->CreateEntity("Green Square");
 		square.AddComponent<SpriteRendererComponent>(glm::vec4{0.0f, 1.0f, 0.0f, 1.0f});
+
+		auto redSquare = m_ActiveScene->CreateEntity("Red Square");
+		redSquare.AddComponent<SpriteRendererComponent>(glm::vec4{ 1.0f, 0.0f, 0.0f, 1.0f });
 
 		m_SquareEntity = square;
 
